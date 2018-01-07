632c53d70507fbf22ccae88af8cfa513539a7f33
Kei Ito <kei.itof@gmail.com>
2018-01-07 14:00:44 +0900

    split plural rules

--- src/-translator/index.js
+++ src/-translator/index.js
@@ -1,115 +1,10 @@
-module.exports = class Translator extends Map {
-	// http://www.unicode.org/cldr/charts/latest/supplemental/language_plural_rules.html
-	// https://developer.mozilla.org/docs/Mozilla/Localization/Localization_and_Plurals
-	// https://hg.mozilla.org/integration/autoland/file/tip/intl/locale/PluralForm.jsm
-	// https://github.com/mozilla/gecko-dev/blob/master/intl/locale/PluralForm.jsm
-	static pluralFunction(rule) {
-		switch (rule) {
-		case 0:
-			return () => {
-				return 0;
-			};
-		case 1:
-			return (n) => {
-				return n === 1 ? 0 : 1;
-			};
-		case 2:
-			return (n) => {
-				return 1 < n ? 1 : 0;
-			};
-		case 3:
-			return (n) => {
-				return n % 10 === 0 ? 0 : n % 10 === 1 && n % 100 !== 11 ? 1 : 2;
-			};
-		case 4:
-			return (n) => {
-				return n === 1 || n === 11 ? 0 : n === 2 || n === 12 ? 1 : isIn(n, 3, 10) || isIn(n, 13, 19) ? 2 : 3;
-			};
-		case 5:
-			return (n) => {
-				return n === 1 ? 0 : n === 0 || isIn(n % 100, 1, 19) ? 1 : 2;
-			};
-		case 6:
-			return (n) => {
-				const d1 = n % 100;
-				const d2 = d1 % 10;
-				return d2 === 1 && d1 !== 11 ? 0 : d2 === 0 || isIn(d1, 10, 20) ? 1 : 2;
-			};
-		case 7:
-			return (n) => {
-				const d1 = n % 100;
-				const d2 = d1 % 10;
-				return d2 === 1 && d1 !== 11 ? 0 : isIn(d2, 2, 4) && !isIn(d1, 12, 14) ? 1 : 2;
-			};
-		case 8:
-			return (n) => {
-				return n === 1 ? 0 : isIn(n, 2, 4) ? 1 : 2;
-			};
-		case 9:
-			return (n) => {
-				return n === 1 ? 0 : isIn(n % 10, 2, 4) && !isIn(n % 100, 12, 14) ? 1 : 2;
-			};
-		case 10:
-			return (n) => {
-				const d = n % 100;
-				return d === 1 ? 0 : d === 2 ? 1 : d === 3 || d === 4 ? 2 : 3;
-			};
-		case 11:
-			return (n) => {
-				return n === 1 ? 0 : n === 2 ? 1 : isIn(n, 3, 6) ? 2 : isIn(n, 7, 10) ? 3 : 4;
-			};
-		case 12:
-			return (n) => {
-				const d = n % 100;
-				return n === 0 ? 0 : n === 1 ? 1 : n === 2 ? 2 : isIn(d, 3, 10) ? 3 : isIn(d, 11, 99) ? 4 : 5;
-			};
-		case 13:
-			return (n) => {
-				const d = n % 100;
-				return n === 1 ? 0 : n === 0 || isIn(d, 1, 10) ? 1 : isIn(d, 11, 19) ? 2 : 3;
-			};
-		case 14:
-			return (n) => {
-				const d = n % 10;
-				return d === 1 ? 0 : d === 2 ? 1 : 2;
-			};
-		case 15:
-			return (n) => {
-				return n % 10 === 1 && n !== 11 ? 0 : 1;
-			};
-		case 16:
-			return (n) => {
-				if (n === 1) {
-					return 0;
-				} else {
-					const dd = n % 100;
-					const d1 = dd % 10;
-					const d2 = (dd - d1) / 10;
-					const not179 = d2 !== 1 && d2 !== 7 && d2 !== 9;
-					if (d1 === 1 && not179) {
-						return 1;
-					} else if (d1 === 2 && not179) {
-						return 2;
-					} else if ((d1 === 3 || d1 === 4 || d1 === 9) && not179) {
-						return 3;
-					} else if (n !== 0 && n % 1000000 === 0) {
-						return 4;
-					} else {
-						return 5;
-					}
-				}
-			};
-		case 17:
-			return (n) => {
-				return n === 0 ? 0 : 1;
-			};
-		default:
-			throw new Error('Unknown rule');
-		}
-	}
+const defaultPluralFunctions = require('../plural-rules');
 
-	constructor(entries) {
+module.exports = class Translator extends Map {
+
+	constructor(entries, pluralFunctions = defaultPluralFunctions) {
 		super();
+		this.pluralFunctions = pluralFunctions;
 		if (entries) {
 			this.load(entries);
 		}
@@ -145,7 +40,10 @@ module.exports = class Translator extends Map {
 
 	use(lang) {
 		this.lang = lang;
-		this.pluralFunction = Translator.pluralFunction(this.langs[lang]);
+		this.pluralFunction = this.pluralFunctions[this.langs[lang]];
+		if (!this.pluralFunction) {
+			throw new Error(`Invalid plural rule: ${this.langs[lang]}`);
+		}
 		this.clear();
 		const translations = this.translations[lang];
 		if (this.phrases) {
@@ -173,7 +71,3 @@ module.exports = class Translator extends Map {
 	}
 
 };
-
-function isIn(x, min, max) {
-	return min <= x && x <= max;
-}
