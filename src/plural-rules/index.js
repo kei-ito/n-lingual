e2fc188d55a956f359021673054613a29c5d3fa9
Kei Ito <kei.itof@gmail.com>
2018-06-01 15:06:54 +0900

    use arrow-func

--- src/plural-rules/index.js
+++ src/plural-rules/index.js
@@ -2,25 +2,14 @@
 // https://developer.mozilla.org/docs/Mozilla/Localization/Localization_and_Plurals
 // https://hg.mozilla.org/integration/autoland/file/tip/intl/locale/PluralForm.jsm
 // https://github.com/mozilla/gecko-dev/blob/master/intl/locale/PluralForm.jsm
+const isIn = (x, min, max) => min <= x && x <= max;
 exports.pluralRules = [
-	() => {
-		return 0;
-	},
-	(n) => {
-		return n === 1 ? 0 : 1;
-	},
-	(n) => {
-		return 1 < n ? 1 : 0;
-	},
-	(n) => {
-		return n % 10 === 0 ? 0 : n % 10 === 1 && n % 100 !== 11 ? 1 : 2;
-	},
-	(n) => {
-		return n === 1 || n === 11 ? 0 : n === 2 || n === 12 ? 1 : isIn(n, 3, 10) || isIn(n, 13, 19) ? 2 : 3;
-	},
-	(n) => {
-		return n === 1 ? 0 : n === 0 || isIn(n % 100, 1, 19) ? 1 : 2;
-	},
+	() => 0,
+	(n) => n === 1 ? 0 : 1,
+	(n) => 1 < n ? 1 : 0,
+	(n) => n % 10 === 0 ? 0 : n % 10 === 1 && n % 100 !== 11 ? 1 : 2,
+	(n) => n === 1 || n === 11 ? 0 : n === 2 || n === 12 ? 1 : isIn(n, 3, 10) || isIn(n, 13, 19) ? 2 : 3,
+	(n) => n === 1 ? 0 : n === 0 || isIn(n % 100, 1, 19) ? 1 : 2,
 	(n) => {
 		const d1 = n % 100;
 		const d2 = d1 % 10;
@@ -31,19 +20,13 @@ exports.pluralRules = [
 		const d2 = d1 % 10;
 		return d2 === 1 && d1 !== 11 ? 0 : isIn(d2, 2, 4) && !isIn(d1, 12, 14) ? 1 : 2;
 	},
-	(n) => {
-		return n === 1 ? 0 : isIn(n, 2, 4) ? 1 : 2;
-	},
-	(n) => {
-		return n === 1 ? 0 : isIn(n % 10, 2, 4) && !isIn(n % 100, 12, 14) ? 1 : 2;
-	},
+	(n) => n === 1 ? 0 : isIn(n, 2, 4) ? 1 : 2,
+	(n) => n === 1 ? 0 : isIn(n % 10, 2, 4) && !isIn(n % 100, 12, 14) ? 1 : 2,
 	(n) => {
 		const d = n % 100;
 		return d === 1 ? 0 : d === 2 ? 1 : d === 3 || d === 4 ? 2 : 3;
 	},
-	(n) => {
-		return n === 1 ? 0 : n === 2 ? 1 : isIn(n, 3, 6) ? 2 : isIn(n, 7, 10) ? 3 : 4;
-	},
+	(n) => n === 1 ? 0 : n === 2 ? 1 : isIn(n, 3, 6) ? 2 : isIn(n, 7, 10) ? 3 : 4,
 	(n) => {
 		const d = n % 100;
 		return n === 0 ? 0 : n === 1 ? 1 : n === 2 ? 2 : isIn(d, 3, 10) ? 3 : isIn(d, 11, 99) ? 4 : 5;
@@ -56,9 +39,7 @@ exports.pluralRules = [
 		const d = n % 10;
 		return d === 1 ? 0 : d === 2 ? 1 : 2;
 	},
-	(n) => {
-		return n % 10 === 1 && n !== 11 ? 0 : 1;
-	},
+	(n) => n % 10 === 1 && n !== 11 ? 0 : 1,
 	(n) => {
 		if (n === 1) {
 			return 0;
@@ -80,11 +61,5 @@ exports.pluralRules = [
 			}
 		}
 	},
-	(n) => {
-		return n === 0 ? 0 : 1;
-	},
+	(n) => n === 0 ? 0 : 1,
 ];
-
-function isIn(x, min, max) {
-	return min <= x && x <= max;
-}
